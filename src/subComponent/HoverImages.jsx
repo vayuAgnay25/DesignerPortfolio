66a111ef7747f5c723a7b071c241a6350dddb06d
vayu <Vayuswaraj2212521@gmail.com>
2025-12-31 22:16:56 +0530

    b

--- src/subComponent/HoverImages.jsx
+++ src/subComponent/HoverImages.jsx
@@ -4,10 +4,12 @@ import I2 from "../assets/imageHover/cos.png"
 import I3 from "../assets/imageHover/sec.png"
 import I4 from "../assets/imageHover/cosec.png"
 import I5 from "../assets/imageHover/cot.png"
+import { useState } from "react";
 
 let imageArr = [I1,I2,I4,I3,I5];
 
 export default function HoverImages() {
+    const [activeIndex, setActiveIndex] = useState(null);
     return (
         <>
             <div className="GlanceLine">
@@ -15,7 +17,13 @@ export default function HoverImages() {
             </div>
             <div className="imageBunch">
                 {
-                    imageArr.map((img,idx)=><div id={`image${idx}`} className="image"><img src={img} /></div>)
+                    imageArr.map((img,idx)=><div id={`image${idx}`} className="image">
+                        <img src={img} 
+                            className={`imageCollage ${activeIndex === idx ? "active" : ""}`}
+                            onMouseEnter={() => setActiveIndex(idx)}
+                            onMouseLeave={() => setActiveIndex(null)}   
+                        />
+                    </div>)
                 }
             </div>
         </>
