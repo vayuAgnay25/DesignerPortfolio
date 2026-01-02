a499dc913a8396542848b37f62a5e4cd2f8abef4
vayu <Vayuswaraj2212521@gmail.com>
2026-01-02 13:43:37 +0530

    s

--- src/subComponent/Galery.jsx
+++ src/subComponent/Galery.jsx
@@ -1,16 +1,17 @@
 import "./GaleryGrid.css";
 import I1 from '../assets/Gallery/D1.jpg';
-import I2 from '../assets/Gallery/D2.jpg';
 import I3 from '../assets/Gallery/D3.jpg';
-import I4 from '../assets/Gallery/D4.jpg';
 import I5 from '../assets/Gallery/D5.jpg';
-import I6 from '../assets/Gallery/D6.jpg';
 import I7 from '../assets/Gallery/D7.jpg';
 import I8 from '../assets/Gallery/D8.jpg';
 import I9 from '../assets/Gallery/D9.jpg';
 import I10 from '../assets/Gallery/D10.jpg';
-import I11 from '../assets/Gallery/D11.jpg';
 import I12 from '../assets/Gallery/D12.jpg';
+
+import I2 from '../assets/Gallery/D2.JPG';
+import I4 from '../assets/Gallery/D4.JPG';
+import I6 from '../assets/Gallery/D6.JPG';
+import I11 from '../assets/Gallery/D11.JPG';
 
 const imageList = [I1,I2,I3,I4,I5,I6,I7,I8,I9,I10,I11,I12]
 
